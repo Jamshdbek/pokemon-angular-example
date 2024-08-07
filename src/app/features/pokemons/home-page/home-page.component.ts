a3886b40c2c454687540201ba00df94cfbbc4237
jamshidbekd664@gmail.com <jamshidbekd664@gmail.com>
2024-08-07 17:25:54 +0500

    adding store-signal and adding -  push query stores

--- src/app/features/pokemons/home-page/home-page.component.ts
+++ src/app/features/pokemons/home-page/home-page.component.ts
@@ -13,18 +13,24 @@ import { PokemonDetail } from '../pokemon.type';
 import * as PokemonAction from '../store/pokemon.action';
 import * as PokemonSelector from '../store/pokemon.selector';
 import { Router, RouterModule } from '@angular/router';
+import { PokemonSignalStore } from '../signalStore/pokemon.store';
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [CardComponent, CommonModule, RouterModule],
+  providers: [PokemonSignalStore],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent implements OnInit {
   router = inject(Router);
+  // classic store
   store = inject(Store);
-
+  // signal store
+  public pokemonSignalStore = inject(PokemonSignalStore);
+  // observables
+  public pokemonDataBySignal = this.pokemonSignalStore.pokemon;
   public pokemonList$: Observable<PokemonDetail[]> = this.store.select(
     PokemonSelector.selectAllPokemonDetail
   );
@@ -33,11 +39,15 @@ export class HomePageComponent implements OnInit {
     PokemonSelector.selectPokemonError
   );
   public offset = signal(0);
-
+  constructor() {
+    console.log(this.pokemonSignalStore.pokemon, 'pokemon data');
+  }
   ngOnInit(): void {
     this.store.dispatch(
       PokemonAction.loadPokemon({ offset: this.offset(), limit: 6 })
     );
+    // fetch request signal ----------------------------------------------
+    this.pokemonSignalStore.loadPokemonQuery({ offset: 0, limit: 6 });
   }
 
   handleShowMore(): void {
---
+++ src/app/features/pokemons/signalStore/pokemon.store.ts
@@ -0,0 +1,44 @@
+import { inject } from '@angular/core';
+import { PokemonList } from '../pokemon.type';
+import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { PokemonService } from '../pokemon.service';
+import { distinctUntilChanged, pipe, switchMap, tap } from 'rxjs';
+import { tapResponse } from '@ngrx/operators';
+type PokemonStoreSignal = {
+  pokemon: PokemonList[];
+  isLoading: boolean;
+  error: string;
+};
+
+const initialState: PokemonStoreSignal = {
+  pokemon: [],
+  isLoading: false,
+  error: '',
+};
+
+export const PokemonSignalStore = signalStore(
+  withState(initialState),
+  withMethods((store, apiService = inject(PokemonService)) => ({
+    loadPokemonQuery: rxMethod(
+      pipe(
+        distinctUntilChanged(),
+        tap(() => patchState(store, { isLoading: true })),
+        switchMap((query) => {
+          return apiService.getPokemonList(0, 10).pipe(
+            tapResponse({
+              next: (pokemon: PokemonList[]) => {
+                console.log(pokemon, 'pokemon overdriwft');
+                return patchState(store, { pokemon, isLoading: false });
+              },
+              error: (err: string) => {
+                patchState(store, { isLoading: false });
+                console.error(err);
+              },
+            })
+          );
+        })
+      )
+    ),
+  }))
+);
